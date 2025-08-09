aa229734a1b372101f1beb4c68a757d03ff0b012
SykoZzz <gabrielomchos88@gmail.com>
2025-08-09 00:27:51 -0500

    Agregar modelo Mascota

---
+++ Models/Mascota.cs
@@ -0,0 +1,12 @@
+namespace Veterinaria.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public string Raza { get; set; }
+        public int Edad { get; set; }
+        public string NombreDueño { get; set; }
+    }
+}
